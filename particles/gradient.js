e3d9bc6c990d658af3cad89261eddef581b26701
riverya4life <111198704+riverya4life@users.noreply.github.com>
2023-03-11 19:54:26 +0200

    Add files via upload

---
+++ particles/gradient.js
@@ -0,0 +1,15 @@
+var text = "FistashkinBot - Discord Bot!";
+var delay = 75; // cкорость
+var elem = document.getElementById("result");
+ 
+var print_text = function(text, elem, delay) {
+    if(text.length > 0) {
+        elem.innerHTML += text[0];
+        setTimeout(
+            function() {
+                print_text(text.slice(1), elem, delay); 
+            }, delay
+        );
+    }
+}
+print_text(text, elem, delay);
